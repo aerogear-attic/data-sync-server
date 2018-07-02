cb9390e87b87d4e7c38a6e2e09d45c4e4490e6d1
Dara Hayes <dara.hayes@redhat.com>
2018-07-02 16:37:29 +0100

    refactor: dataSourceParser and resolverMaker

--- server/lib/dataSourceParser.js
+++ server/lib/dataSourceParser.js
@@ -1,30 +1,29 @@
 const _ = require('lodash')
-const {Client} = require('pg')
+const { Client } = require('pg')
 
-module.exports = function (dataSources, connect = true) {
-  let dataSourceClients = {}
-  let dataSourceTypes = {}
+module.exports = function (dataSourceDefs, connect = true) {
+  let dataSources = {}
   // connect to any datasources with persistent connections
-  _.forEach(dataSources, (value, key) => {
+  _.forEach(dataSourceDefs, (dataSource, dataSourceName) => {
     let isHandled = false
-    if (value.type === 'postgres') {
-      dataSourceClients[key] = new Client(value.config)
-      if (connect) {
-        dataSourceClients[key].connect()
-      }
+
+    if (dataSource.type === 'postgres') {
+      dataSources[dataSourceName] = createPostgresDataSource(dataSource.config, connect)
       isHandled = true
     }
 
-    if (isHandled) {
-      dataSourceTypes[key] = value.type
-    } else {
-      throw new Error('Unhandled data source type: ' + value.type)
+    if (!isHandled) {
+      throw new Error('Unhandled data source type: ' + dataSource.type)
     }
   })
 
-  return {
-    dataSourceTypes,
-    // didn't want to set 'type' property on pg.Client object
-    dataSourceClients
+  return dataSources
+}
+
+function createPostgresDataSource (config, connect = true) {
+  let client = new Client(config)
+  if (connect) {
+    client.connect()
   }
+  return { client, type: 'postgres' }
 }
