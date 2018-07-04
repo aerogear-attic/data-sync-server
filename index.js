ff6a0c0549aaa98a583353dd084a4ab32b1e2fe1
Dara Hayes <dara.hayes@redhat.com>
2018-07-04 16:35:44 +0100

    fix: tidy up old scripts

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require('./server/config')
 let { graphQLConfig, graphiqlConfig, postgresConfig } = config
 let { port } = config.server
 
-const models = require('./models/index')(postgresConfig)
+const models = require('./sequelize/models/index')(postgresConfig)
 models.sequelize.sync().then(async () => {
   const server = await require('./server/server')({ graphQLConfig, graphiqlConfig }, models)
   await server.listen(port)
--- scripts/sequelize_models.js
+++
@@ -1,6 +0,0 @@
-const postgresConfig = require('../server/config').postgresConfig
-
-require('../models')(postgresConfig).sequelize.sync().then(() => {
-  console.log('sequelize done')
-  process.exit(0)
-})
