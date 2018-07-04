78dbd5b37f173db5cb34be76a6a1d97678a722c0
Dara Hayes <dara.hayes@redhat.com>
2018-07-04 17:09:38 +0100

    fix: force drop based on env variable

--- sequelize/sync_models.js
+++ sequelize/sync_models.js
@@ -1,6 +1,8 @@
 const postgresConfig = require('../server/config').postgresConfig
 
-require('../sequelize/models')(postgresConfig).sequelize.sync({force: true}).then(() => {
+const forceDrop = process.env.FORCE_DROP === 'true'
+
+require('../sequelize/models')(postgresConfig).sequelize.sync({force: forceDrop}).then(() => {
   console.log('sequelize done')
   process.exit(0)
 })
