2f2143a1100428be65610e44d68fe98768622dda
Ali Ok <aliok@apache.org>
2018-07-04 12:17:06 +0300

    Minor fixes in example resolvers

--- examples/create_example_data.sql
+++ examples/create_example_data.sql
@@ -19,7 +19,7 @@ INSERT INTO "GraphQLSchemas" ("id","schema","createdAt","updatedAt") VALUES (DEF
         content: String,
       ): Note
       updateNote(
-        _id: String,
+        id: String,
         title: String,
         content: String
       ): Note
@@ -38,8 +38,8 @@ INSERT INTO "GraphQLSchemas" ("id","schema","createdAt","updatedAt") VALUES (DEF
       noteCreated: Note
     }','2018-07-03 09:49:57.062 +00:00','2018-07-03 09:49:57.062 +00:00') RETURNING *;
 
-INSERT INTO "Resolvers" ("id","type","field","requestMapping","responseMapping","createdAt","updatedAt","DataSourceId") VALUES (DEFAULT,'Query','readNote','{"operation": "findOne","query": {"_id": "{{context.arguments._id}}"}}','{{toJSON context.result}}','2018-07-03 10:11:30.054 +00:00','2018-07-03 10:11:30.054 +00:00','1') RETURNING *;
+INSERT INTO "Resolvers" ("id","type","field","requestMapping","responseMapping","createdAt","updatedAt","DataSourceId") VALUES (DEFAULT,'Query','readNote','{"operation": "findOne","query": {"_id": "{{context.arguments.id}}"}}','{{toJSON context.result}}','2018-07-03 10:11:30.054 +00:00','2018-07-03 10:11:30.054 +00:00','1') RETURNING *;
 INSERT INTO "Resolvers" ("id","type","field","requestMapping","responseMapping","createdAt","updatedAt","DataSourceId") VALUES (DEFAULT,'Query','listNotes','{"operation": "find","query": {}}','{{toJSON context.result}}','2018-07-03 10:11:30.054 +00:00','2018-07-03 10:11:30.054 +00:00','1') RETURNING *;
 INSERT INTO "Resolvers" ("id","type","field","requestMapping","responseMapping","createdAt","updatedAt","DataSourceId") VALUES (DEFAULT,'Mutation','createNote','{"operation": "insert","doc": {"title": "{{context.arguments.title}}","content": "{{context.arguments.content}}"}}','{{toJSON context.result}}','2018-07-03 10:11:30.054 +00:00','2018-07-03 10:11:30.054 +00:00','1') RETURNING *;
-INSERT INTO "Resolvers" ("id","type","field","requestMapping","responseMapping","createdAt","updatedAt","DataSourceId") VALUES (DEFAULT,'Mutation','updateNote','{"operation": "update","query": {"_id": "{{context.arguments._id}}"}, "update":{"title": "{{context.arguments.title}}","content": "{{context.arguments.content}}"},"options":{}}','{{toJSON context.result}}','2018-07-03 10:11:30.054 +00:00','2018-07-03 10:11:30.054 +00:00','1') RETURNING *;
-INSERT INTO "Resolvers" ("id","type","field","requestMapping","responseMapping","createdAt","updatedAt","DataSourceId") VALUES (DEFAULT,'Mutation','deleteNote','{"operation": "remove","query": {"_id": "{{context.arguments._id}}"},"options":{}}','{{toJSON context.result}}','2018-07-03 10:11:30.054 +00:00','2018-07-03 10:11:30.054 +00:00','1') RETURNING *;
+INSERT INTO "Resolvers" ("id","type","field","requestMapping","responseMapping","createdAt","updatedAt","DataSourceId") VALUES (DEFAULT,'Mutation','updateNote','{"operation": "update","query": {"_id": "{{context.arguments.id}}"}, "update":{"title": "{{context.arguments.title}}","content": "{{context.arguments.content}}"},"options":{}}','{{toJSON context.result}}','2018-07-03 10:11:30.054 +00:00','2018-07-03 10:11:30.054 +00:00','1') RETURNING *;
+INSERT INTO "Resolvers" ("id","type","field","requestMapping","responseMapping","createdAt","updatedAt","DataSourceId") VALUES (DEFAULT,'Mutation','deleteNote','{"operation": "remove","query": {"_id": "{{context.arguments.id}}"},"options":{}}','{{toJSON context.result}}','2018-07-03 10:11:30.054 +00:00','2018-07-03 10:11:30.054 +00:00','1') RETURNING *;
