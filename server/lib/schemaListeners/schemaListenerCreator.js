2e278679e030a87722eeeb98270cf1bfe5ef5f52
Ali Ok <aliok@apache.org>
2018-07-05 13:57:11 +0300

    Rename `callback` to something meaningful

--- server/lib/schemaListeners/schemaListenerCreator.js
+++ server/lib/schemaListeners/schemaListenerCreator.js
@@ -1,12 +1,12 @@
 const listeners = require('./listeners')
 
-module.exports = function (schemaListenerConfig, callback) {
+module.exports = function (schemaListenerConfig, onReceive) {
   for (let type of Object.keys(listeners)) {
     if (type === schemaListenerConfig.type) {
       const listener = listeners[type]
 
       if (listener && typeof listener === 'function') {
-        listener(schemaListenerConfig.config, callback)
+        listener(schemaListenerConfig.config, onReceive)
         return // return to end the loop
       } else {
         throw new Error(`Schema listener for ${type} is not a function`)
