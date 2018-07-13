7ee85c00588e0d1f0b70918c6b490e16085dd1f9
Dara Hayes <dara.hayes@redhat.com>
2018-07-13 09:43:40 +0100

    fix: simplify memeolist schema

--- sequelize/seeders/20180711143600-memeolist-example.js
+++ sequelize/seeders/20180711143600-memeolist-example.js
@@ -16,33 +16,18 @@ const datasources = [
 const notesSchema = {
   name: 'default',
   schema: `
-  
-  type Profile {
-    id: ID! @isUnique
-    email: String! @isUnique
-    displayName: String!
-    biography: String!
-    avatarUrl: String!
-    memes: [Meme]!
-  }
-  
+
   type Meme {
     id: ID! @isUnique
     photoUrl: String!
-    ownerId: String!
   }
   
   type Query {
-    allProfiles:[Profile!]!
-    profile(email: String!):Profile
     allMemes:[Meme!]!
   }
   
   type Mutation {
-    createProfile(email: String!, displayName: String!, biography: String!, avatarUrl: String!):Profile!
-    updateProfile(id: ID!, email: String!, displayName: String!, biography: String!, avatarUrl: String!):Profile
-    deleteProfile(id: ID!):Boolean!
-    createMeme(ownerId: String! photoUrl: String!):Meme!
+    createMeme(photoUrl: String!):Meme!
   }
   
   type Subscription {
@@ -72,83 +57,10 @@ const resolvers = [
       "operation": "insert",
       "doc": {
         "_type":"meme",
-        "photoUrl": "{{context.arguments.photoUrl}}",
-        "ownerId": "{{context.arguments.ownerId}}"
+        "photoUrl": "{{context.arguments.photoUrl}}"
       }
     }`,
     responseMapping: '{{ toJSON (convertNeDBIds context.result) }}',
-    createdAt: time,
-    updatedAt: time
-  },
-  {
-    type: 'Profile',
-    field: 'memes',
-    DataSourceId: 2,
-    requestMapping: '{"operation": "find", "query": {"_type":"meme", "ownerId": "{{context.parent.id}}"}}',
-    responseMapping: '{{ toJSON (convertNeDBIds context.result) }}',
-    createdAt: time,
-    updatedAt: time
-  },
-  {
-    type: 'Query',
-    field: 'allProfiles',
-    DataSourceId: 2,
-    requestMapping: '{"operation": "find", "query": {"_type":"profile"}}',
-    responseMapping: '{{ toJSON (convertNeDBIds context.result) }}',
-    createdAt: time,
-    updatedAt: time
-  },
-  {
-    type: 'Mutation',
-    field: 'createProfile',
-    DataSourceId: 2,
-    requestMapping: `{
-      "operation": "insert",
-      "doc": {
-        "_type":"profile",
-        "email": "{{context.arguments.email}}",
-        "displayName": "{{context.arguments.displayName}}",
-        "biography": "{{context.arguments.biography}}",
-        "avatarUrl": "{{context.arguments.avatarUrl}}",
-        "memes": []
-      }
-    }`,
-    responseMapping: '{{ toJSON (convertNeDBIds context.result) }}',
-    createdAt: time,
-    updatedAt: time
-  },
-  {
-    type: 'Mutation',
-    field: 'updateProfile',
-    DataSourceId: 2,
-    requestMapping: `{
-      "operation": "update",
-      "query": {"_type":"profile", "_id": "{{context.arguments.id}}" },
-      "update": { 
-        "$set": {
-          "email": "{{context.arguments.email}}",
-          "displayName": "{{context.arguments.displayName}}", 
-          "biography": "{{context.arguments.biography}}",    
-          "avatarUrl": "{{context.arguments.avatarUrl}}"
-        }    
-      },
-      "options": {
-        "returnUpdatedDocs": true
-      }
-    }`,
-    responseMapping: '{{ toJSON (convertNeDBIds context.result) }}',
-    createdAt: time,
-    updatedAt: time
-  },
-  {
-    type: 'Mutation',
-    field: 'deleteProfile',
-    DataSourceId: 2,
-    requestMapping: `{
-      "operation": "remove",
-      "query": {"_type":"profile", "_id": "{{context.arguments.id}}" }
-    }`,
-    responseMapping: '{{toBoolean context.result}}',
     createdAt: time,
     updatedAt: time
   }
