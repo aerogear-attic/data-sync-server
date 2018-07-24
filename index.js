35a4a637b59ff69142af75cc7505430896f00cb3
Dara Hayes <dara.hayes@redhat.com>
2018-07-24 16:41:55 +0100

    fix: add abstraction layer over pubsub mechanism

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const config = require('./server/config')
 const { log } = require('./server/lib/util/logger')
+const PubSub = require('./server/lib/pubsubNotifiers/pubsubNotifier')
 
-let { postgresConfig } = config
+let { pubsubConfig, postgresConfig } = config
 let { port } = config.server
 
 process.on('uncaughtException', err => console.error('uncaught exception:', err))
@@ -10,8 +11,9 @@ process.on('unhandledRejection', error => console.error('unhandled rejection:', 
 async function start () {
   const models = require('./sequelize/models/index')(postgresConfig)
   await models.sequelize.sync({ logging: false })
+  const pubsub = PubSub(pubsubConfig)
 
-  const server = await require('./server/server')(config, models)
+  const server = await require('./server/server')(config, models, pubsub)
   await server.listen(port)
   log.info(`Server is now running on http://localhost:${port}`)
 }
--- server/lib/schemaParser.js
+++ server/lib/schemaParser.js
@@ -4,13 +4,9 @@ const dataSourceParser = require('./datasources/dataSourceParser')
 const resolverMapper = require('./resolvers/resolverMapper')
 const subscriptionsMapper = require('./subscriptions/subscriptionMapper')
 
-const PubSub = require('./pubsubNotifiers').InMemory
-
-module.exports = function (schemaString, dataSourcesJson, resolverMappingsJson, subscriptionMappingsJson) {
-  const pubsub = PubSub()
-
+module.exports = function (schemaString, dataSourcesJson, resolverMappingsJson, subscriptionMappingsJson, pubsub) {
   const dataSources = dataSourceParser(dataSourcesJson)
-  const subscriptionResolvers = subscriptionsMapper(subscriptionMappingsJson, pubsub) // { Subscriptions: {}}
+  const subscriptionResolvers = subscriptionsMapper(subscriptionMappingsJson, pubsub)
   const dataResolvers = resolverMapper(dataSources, resolverMappingsJson, pubsub)
 
   const resolvers = {...subscriptionResolvers, ...dataResolvers}
