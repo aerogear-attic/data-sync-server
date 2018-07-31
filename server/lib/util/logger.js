004898cfc12a7f30cd60968a64e7464e0e78dccc
Ali Ok <aliok@apache.org>
2018-07-31 15:47:22 +0300

    AEROGEAR-7727 provide a way to enable/disable audit logging

--- server/lib/util/logger.js
+++ server/lib/util/logger.js
@@ -1,18 +1,22 @@
 const log = require('pino')()
 const auditLogger = log.child({tag: 'AUDIT'})
 
+const auditLogEnabled = process.env.AUDIT_LOGGING !== 'false' && process.env.AUDIT_LOGGING !== false
+
 function auditLog (success, request, info, parent, args, msg) {
-  auditLogger.info({
-    msg: msg || '',
-    requestId: request.id,
-    operationType: info.operation.operation,
-    fieldName: info.fieldname,
-    parentTypeName: info.parentType.name,
-    path: buildPath(info.path),
-    success: success,
-    parent: parent,
-    arguments: args
-  })
+  if (auditLogEnabled) {
+    auditLogger.info({
+      msg: msg || '',
+      requestId: request.id,
+      operationType: info.operation.operation,
+      fieldName: info.fieldname,
+      parentTypeName: info.parentType.name,
+      path: buildPath(info.path),
+      success: success,
+      parent: parent,
+      arguments: args
+    })
+  }
 }
 
 // builds path for a GraphQL operation
