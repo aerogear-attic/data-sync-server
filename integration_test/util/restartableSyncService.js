eff7677603b3ab97ec786078f758ea9666024918
Dara Hayes <dara.hayes@redhat.com>
2018-08-01 09:49:34 +0100

    fix: comment about restart delay in RestartableSyncService

--- integration_test/util/restartableSyncService.js
+++ integration_test/util/restartableSyncService.js
@@ -1,12 +1,17 @@
 const DataSyncService = require('../../DataSyncService')
 const stoppable = require('stoppable')
 
+// There's a bug somewhere in the server initialization/start sequence
+// Even though we await service.initialize() some things aren't always ready
+// Like underlying pubsub layer etc.
+const RESTART_DELAY = 200
+
 class RestartableSyncService extends DataSyncService {
   async start () {
     this.app.server = stoppable(this.app.server, 0)
     await this.app.server.listen(this.port)
     this.log.info(`Server is now running on http://localhost:${this.port}`)
-    await new Promise(resolve => setTimeout(resolve, 200))
+    await new Promise(resolve => setTimeout(resolve, RESTART_DELAY))
   }
 
   async restart () {
@@ -15,7 +20,7 @@ class RestartableSyncService extends DataSyncService {
     this.app.server.stop()
     await this.initialize()
     await this.start()
-    await new Promise(resolve => setTimeout(resolve, 200))
+    await new Promise(resolve => setTimeout(resolve, RESTART_DELAY))
   }
 }
 
