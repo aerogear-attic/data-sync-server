690e4997fdecead79ebc7d0b53dede1303b1bdd4
Wojciech Trocki <wtrocki@redhat.com>
2018-08-21 16:58:30 +0100

    WIP - hasRole

--- server/lib/schemaDirectives/hasRole.js
+++ server/lib/schemaDirectives/hasRole.js
@@ -8,6 +8,8 @@ class HasRoleDirective extends SchemaDirectiveVisitor {
     const { role } = this.args
     field.resolve = async function (root, args, context, info) {
       log.info('checking has role directive', role)
+      log.info('Printing user', context.request.session)
+      // context.request.kauth.grant.access_token.hasRealmRole(role)
       // TODO - figure out how to check does the logged in user have the correct role
       // using the keycloak-connect library
       const result = await resolve.apply(this, [root, args, context, info])
