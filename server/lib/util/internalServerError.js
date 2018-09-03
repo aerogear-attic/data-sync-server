ba77b0872d3a7e3b84a05a6168ca87914f67bafb
Dara Hayes <dara.hayes@redhat.com>
2018-09-03 15:10:46 +0100

    fix: add test for internalServerError module

--- server/lib/util/internalServerError.js
+++ server/lib/util/internalServerError.js
@@ -1,12 +1,12 @@
 const { ApolloError } = require('apollo-server-express')
 const uuid = require('uuid')
 
-const code = 'INTERNAL_SERVER_ERROR'
+const INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR'
 
 function newInternalServerError (context) {
   let errorId = (context && context.request) ? context.request.id : uuid.v4()
   const genericErrorMsg = `An internal server error occurred, please contact the server administrator and provide the following id: ${errorId}`
-  return new ApolloError(genericErrorMsg, code, { errorId })
+  return new ApolloError(genericErrorMsg, INTERNAL_SERVER_ERROR, { id: errorId })
 }
 
 module.exports = newInternalServerError
