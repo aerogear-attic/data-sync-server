934eb5863ccf48e9c5c70b5ade7132baf90ea55c
Vojtěch Sázel <vojtech@sazel.cz>
2018-09-05 10:35:03 +0200

    CircleCI config updated to allow running puppeteer

--- integration_test/util/auth.js
+++ integration_test/util/auth.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 
 async function authenticateKeycloak (username, password) {
   const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+  const page = await browser.newPage({args: ['--no-sandbox', '--disable-setuid-sandbox']})
 
   async function loginProcess () {
     await page.goto('http://localhost:8000/token')
